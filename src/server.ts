e56b5a07aca5d293ab4955ceb530097d84ac9b87
linton <lintonli162@gmail.com>
2024-06-10 20:15:13 +0300

    third commit

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import router from "./Routes";
 const app = express();
 app.use(json());
+app.use("products", router);
 app.listen(3000, () => {
   console.log("App Running....");
 });
